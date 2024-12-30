77009e9bf2f803cbdc50f8e1ffe9106d828f65e8
Mauro Sergio <75049394+MauroSergio10@users.noreply.github.com>
2024-12-30 19:20:05 +0000

    RegisterUser.ts

--- frontend/src/pages/registerUser.tsx
+++ frontend/src/pages/registerUser.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { HtmlHTMLAttributes } from "react";
 import { useState } from "react";
 
 const RegisterUser: React.FC = () => {
@@ -10,19 +10,56 @@ const RegisterUser: React.FC = () => {
     telefones: "",
   });
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCondominio({ ...condominio, [e.target.name]: e.target.value });
+  };
+
+  const onForm = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onForm}>
         <label htmlFor="text">Apartamento</label>
-        <input type="text" name="ap" value={condominio.ap} required />
+        <input
+          type="text"
+          name="ap"
+          value={condominio.ap}
+          required
+          onChange={handleChange}
+        />
         <label htmlFor="propietario">Propietario</label>
-        <input type="text" name="propietario" value={condominio.ap} />
+        <input
+          type="text"
+          name="propietario"
+          value={condominio.ap}
+          required
+          onChange={handleChange}
+        />
         <label htmlFor="administradores">Administradores</label>
-        <input type="text" name="string" value={condominio.administradores} />
+        <input
+          type="text"
+          name="string"
+          value={condominio.administradores}
+          onChange={handleChange}
+        />
         <label htmlFor="email">Email</label>
-        <input type="text" name="email" value={condominio.emails} />
+        <input
+          type="text"
+          name="email"
+          value={condominio.emails}
+          required
+          onChange={handleChange}
+        />
         <label htmlFor="text">Telefone</label>
-        <input type="text" name="telefones" value={condominio.telefones} />
+        <input
+          type="text"
+          name="telefones"
+          value={condominio.telefones}
+          required
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
